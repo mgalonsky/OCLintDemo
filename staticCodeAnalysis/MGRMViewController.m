0f725994ce675a3ad195fee095bd40f7befb34c6
Melissa Galonsky <mgalonsky@cs.hmc.edu>
2014-09-19 11:44:30 -0700

    Starting to work with OCLint.

--- staticCodeAnalysis/MGRMViewController.m
+++ staticCodeAnalysis/MGRMViewController.m
@@ -18,6 +18,21 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSString* temp;
+    NSArray* tempArray;
+    int i = 0;
+    switch (i) {
+        case 0:
+            temp = nil;
+        case 1:
+            tempArray = @[temp];
+            NSLog(@"%d",(int)[tempArray objectAtIndex:0]);
+            break;
+            
+        default:
+            break;
+    }
 }
 
 - (void)didReceiveMemoryWarning
